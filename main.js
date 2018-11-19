99f3049056b0e47ba0e71ee16f0fbadc68a610e8
knarik.mheryan <knarik.mheryan@helpsystems.com>
2018-11-19 11:37:52 +0400

    commit_4: With 'global' declared DOM elements, promise, arrow functions, for-of loop, code improvements.

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 $(function() {
+    const inputsSelectors = {};
     const INIT_PAGE = 1;
     const STATUS_VALID = 'valid';
     const STATUS_INVALID = 'invalid';
@@ -10,10 +11,17 @@ $(function() {
     let totalPages = 1;
     let currentPage = 1;
     let searchData = {};
+    let searchContainer = $('.search-container');
+    let tableBody = $('#table-data');
+    let previousPage = $('.previous');
+    let nextPage = $('.next');
+    let searchInputs = $('form input');
+    const searchFieldsArr = ['firstName', 'lastName', 'fatherName', 'year'];
 
     resetTable();
+    initInputs(searchFieldsArr);
 
-    $('#search').click(function(e) {
+    $('#search').click((e) => {
         e.preventDefault();
         currentPage = INIT_PAGE;
         searchData = collectData();
@@ -23,43 +31,39 @@ $(function() {
 
         if (error.status === STATUS_VALID) {
             showLoadMask();
+            let peoplePromise = getPeoplePromise();
 
-            $.ajax({
-                url: PEOPLE_URL,
-                type: 'GET',
-                contentType: 'application/json',
-                data: searchData,
-                dataType: 'json'
-            }).done(function(data) {
+            peoplePromise.done((data) => {
                 updateTable(data.people);
                 updatePagination(data.totalPages);
                 hideLoadMask();
-            }).fail(function(error) {
+            }).fail((error) => {
                 hideLoadMask();
                 console.log(error);
             });
 
-            $('#search').addClass('disabled').css('pointer-events', 'none');
+            disableItem($('#search'));
 
         } else {
             showErrors(error);
-            $('#search').addClass('disabled').css('pointer-events', 'none');
+            disableItem($('#search'));
         }
     });
 
-    $('form input.form-control').keydown(function() {
+    $('form input.form-control').keydown(() => {
         let data = collectData();
         let error = validate(data);
         cleanErrors();
 
         if (error.status === STATUS_INVALID) {
             showErrors(error);
-            $('#search').addClass('disabled').css('pointer-events', 'none');
+            disableItem($('#search'));
         } else {
-            $('#search').removeClass('disabled').css('pointer-events', 'auto');
+            enableItem($('#search'));
         }
     });
-    $('.pagination').on('click', '.page-item', function (e) {
+
+    $('.pagination').on('click', '.page-item', (e) => {
         e.preventDefault();
 
         if ($(this).hasClass('next')) {
@@ -71,30 +75,20 @@ $(function() {
         }
 
         searchData.page = currentPage;
+        let peoplePromise = getPeoplePromise();
 
-        $.ajax({
-            url: PEOPLE_URL,
-            type: 'GET',
-            contentType: 'application/json',
-            data: searchData,
-            dataType: 'json'
-        }).done(function(data) {
+        peoplePromise.done((data) => {
             updateTable(data.people);
             updatePagination(data.totalPages);
             hideLoadMask();
-        }).fail(function(error) {
+        }).fail((error) => {
             hideLoadMask();
             console.log(error);
         })
     });
 
     function collectData() {
-        let data = {};
-
-        data.firstName = $('input[name=firstName]').val().trim();
-        data.lastName = $('input[name=lastName]').val().trim();
-        data.fatherName = $('input[name=fatherName]').val().trim();
-        data.year = $('input[name=year]').val().trim();
+        let data = getInputsValues(searchFieldsArr);
         data.page = INIT_PAGE;
 
         return data;
@@ -129,19 +123,8 @@ $(function() {
         return error;
     }
 
-    function showErrors(error={}) {
-        error.fields.forEach(function(field) {
-            $('input[name=' + field +']').addClass('is-invalid');
-        });
-    }
-
-    function cleanErrors() {
-        $('form input').removeClass('is-invalid');
-    }
-
     function updateTable(data={}) {
         let rows = '';
-        let tableBody = $('#table-data');
         resetTable();
 
         for (let i=0; i<data.length; i++) {
@@ -151,22 +134,10 @@ $(function() {
         tableBody.append(rows);
     }
 
-    function getRow(rowData={}, n=1) {
-        return '<tr>' +
-            '<td scope="row" data-id="' + rowData.id + '">' + n + '</td>' +
-            '<td>' + rowData.firstName + '</td>' +
-            '<td>' + rowData.lastName + '</td>' +
-            '<td>' + rowData.fatherName + '</td>' +
-            '<td>' + rowData.birthDate + '</td>' +
-            '<td>' + rowData.address.replace(/\s\s+/g, ' ') + '</td>' +
-        '</tr>';
-    }
-
     function updatePagination(pageCount=1) {
         let pages = '';
         let paginationView = $('.pagination');
-        let previousPage = $('.previous');
-        let nextPage = $('.next');
+
         resetPagination();
 
         if (pageCount !== totalPages) {
@@ -182,40 +153,92 @@ $(function() {
             paginationView.append(nextPage);
         }
 
-        $.each(paginationView.find('.page-link'), function() {
-            if (parseInt($(this).text()) === currentPage) {
-                $(this).addClass('selected');
+        for (let item of paginationView.find('.page-link')) {
+            if (parseInt($(item).text()) === currentPage) {
+                $(item).addClass('selected');
             }
-        });
+        }
 
         if (currentPage === INIT_PAGE) {
-            previousPage.addClass('disabled').css('pointer-events', 'none');
+            disableItem(previousPage);
         }
 
         if (currentPage === totalPages) {
-            nextPage.addClass('disabled').css('pointer-events', 'none');
+            disableItem(nextPage);
         }
+    }
+
+    function getPeoplePromise() {
+        return  $.ajax({
+            url: PEOPLE_URL,
+            type: 'GET',
+            contentType: 'application/json',
+            data: searchData,
+            dataType: 'json'
+        });
+    }
+
+    function getRow(rowData={}, n=1) {
+        return '<tr>' +
+            '<td scope="row" data-id="' + rowData.id + '">' + n + '</td>' +
+            '<td>' + rowData.firstName + '</td>' +
+            '<td>' + rowData.lastName + '</td>' +
+            '<td>' + rowData.fatherName + '</td>' +
+            '<td>' + rowData.birthDate + '</td>' +
+            '<td>' + rowData.address.replace(/\s\s+/g, ' ') + '</td>' +
+            '</tr>';
     }
 
     function getPaginationItem(n=1) {
         return  '<li class="page-item"><a class="page-link" href="#' + n + '">' + n  + '</a></li>';
     }
 
+    function initInputs(inputs) {
+        inputs.forEach((field) => {
+            inputsSelectors[field] = $('input[name=' + field +']');
+        });
+    }
+
+    function getInputsValues(inputs) {
+        let data = {};
+        inputs.forEach((field) => {
+            data[field] = $('input[name=' + field +']').val().trim();
+        });
+        return data;
+    }
+
+    function disableItem(item) {
+        item.addClass('disabled').css('pointer-events', 'none');
+    }
+
+    function enableItem(item) {
+        item.removeClass('disabled').css('pointer-events', 'auto');
+    }
+
     function resetTable() {
-        $('#table-data').empty();
+        tableBody.empty();
     }
 
     function resetPagination() {
         $('.pagination li > a').removeClass('selected');
-        $('.pagination li').removeClass('disabled').css('pointer-events', 'auto');
+        enableItem($('.pagination li'));
     }
 
     function showLoadMask() {
-        $('.search-container').addClass('loading-mask');
+        searchContainer.addClass('loading-mask');
     }
 
     function hideLoadMask() {
-        $('.search-container').removeClass('loading-mask');
+        searchContainer.removeClass('loading-mask');
     }
 
+    function showErrors(error={}) {
+        error.fields.forEach((field) => {
+            inputsSelectors[field].addClass('is-invalid');
+        });
+    }
+
+    function cleanErrors() {
+        searchInputs.removeClass('is-invalid');
+    }
 });
